57ba8c3ba27a3e6252b6b8bb0fb5ca93be0596f5
Jess Johnson <jessica.irish.johnson@gmail.com>
2016-03-12 00:07:27 +0000

    attempting to callback

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,7 +8,7 @@ var handlebars  = require('handlebars');
 var fs = require('fs');
 var session = require('express-session');
 var Users = require('../models/users.js');
-
+var Nightclubs = require('../models/nightlife.js');
 
 
 
@@ -56,30 +56,62 @@ var yelp = new Yelp({
 
 app.post('/',  upload.array(), function (req, res, next) {
 		console.log("location:" + req.body["location"]);
-	
+		   var loggedin;
+             if (req.isAuthenticated()) {
+                 loggedin = true;
+             } else {
+                  loggedin = false;
+             }
 	//	console.log("user" + res.user);
 
 		yelp.search({ term: 'nightlife', location: req.body["location"] })
 				.then(function (data) {
 					var biz="<div>"
+					
   					for (var i=0; i< data.businesses.length; i++){
-  						biz+="<div id='biz'><h4><a href='" +data.businesses[i].url +"'>";
-  						biz+= data.businesses[i].name+"</a></h4>";
-  						biz+= "<img src='" + data.businesses[i].image_url+"'>";
-  						biz += data.businesses[i].snippet_text;
+  					    //for each biz id I should find if the id exists. if it doesn't I can assume no one is there. I'm choosing to only create id on check in to save space.
+  					  
+                             var url =  data.businesses[i].url;
+  						     var name = data.businesses[i].name;
+  						     var imgURL = data.businesses[i].image_url;
+  					    	 var snippet = data.businesses[i].snippet_text;
+  					    	 console.log("yo");
+  				    
+                             
+                            var getTheSearch = function (callback){
+                                var partygoers;
+
+  		                        Nightclubs.findOne({ 'id': data.businesses[i].id }, function (err, nightclub) {
+                                    if (err) throw err;
+                                    if(nightclub){
+                                        partygoers = nightclub.users.length;
+                                    //TODO add in what to do if I'M going to the nightclub
+                                    } else {
+                                         partygoers = 0;
+                                    }
+                        
+                                });
+                            }   
+                        
+                            var formatting = function(partygoers){
+                                biz+="<div id='biz'><h4><a href='" + url +"'>";
+                                biz+= name +"</a></h4>";
+  	                            biz+= "<img src='" + imgURL+"'>";
+  	                            biz += snippet;
+                            }
   					}
-  					biz +="</div>"
-  					
-  					
-  					var data = {
-  						nightlife: biz,
-           			}
-           			
-  					fs.readFile('public/index.html', 'utf-8', function(error, source){
-            			 var template = handlebars.compile(source);
-                		 var html = template(data);
-                		 res.send(html);
-               		}); 
+  				         biz +="</div>"
+  				
+  					            //inserts biz info via handlebars
+  				            	var data = {
+  						            nightlife: biz,
+  					            	loggedin: loggedin
+           		            	}
+           		            	fs.readFile('public/index.html', 'utf-8', function(error, source){
+            			             var template = handlebars.compile(source);
+                		            var html = template(data);
+                		             res.send(html);
+               	            	});  
   					
 				})
 			   .catch(function (err) {
@@ -110,7 +142,6 @@ app.route('/auth/twitter/callback')
 
 
 };
-
 
 
 
