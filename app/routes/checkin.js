7fcaf3d8cbab420216c6c6f311f7187d009bd852
Jess Johnson <jessica.irish.johnson@gmail.com>
2016-03-15 20:43:04 +0000

    check for login on checkin page

--- app/routes/checkin.js
+++ app/routes/checkin.js
@@ -10,11 +10,23 @@ var Nightclubs = require('../models/nightlife.js');
 
 
 
+
 module.exports = function (app, passport) {
 
+function isLoggedIn (req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        } else {
+            res.redirect('/login');
+        }
+    }
+
+    
+    
 
 app.route('/checkin/:checkinID')
-  .get(function (req, res) {
+  .get(isLoggedIn, function (req, res) {
+       
        res.sendFile(path + '/public/checkin.html');
       
   })
