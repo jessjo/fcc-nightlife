ffe67d1e3512b54bffa343d6f174a9fcce35bac3
Jess Johnson <jessica.irish.johnson@gmail.com>
2016-03-16 17:10:07 +0000

    remove from nightclub data

--- app/routes/checkin.js
+++ app/routes/checkin.js
@@ -21,13 +21,16 @@ function isLoggedIn (req, res, next) {
         }
     }
 
-function removeFromClub(checkinID){
+function removeFromClub(checkinID, userID){
            Nightclubs.findOne({'id':checkinID}, function(err,Nightclub){
             if (err) throw err;
             if(Nightclub){
-                
+                console.log ("before remove:" +Nightclub.nightclub.users  );
+                var user = Nightclub.nightclub.users.indexOf(userID);
+                Nightclub.nightclub.users.splice(user, 1);
+                console.log ("after remove:" +Nightclub.nightclub.users  );
             } else {
-                
+                console.log ("nightclub not found - error?")
             }
         });
     
@@ -45,6 +48,9 @@ app.route('/checkin/:checkinID')
             if (err) throw err;
             if(User){
                 console.log("Old location:" + User.nightclub.nightclub);
+                if (User.nightclub.nightclub.length >0){
+                    removeFromClub(User.nightclub.nightclub, req.ID);
+                }
                 User.nightclub.nightclub = req.params.checkinID;
                 console.log("New location:" + User.nightclub.nightclub);
                 User.save();
